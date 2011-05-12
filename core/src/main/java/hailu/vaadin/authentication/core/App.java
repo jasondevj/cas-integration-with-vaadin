01058c2190c5e65e38c7395211418230cc40cd5e
Jason <jasondevj@gmail.com>
2011-05-12 15:20:50 +0530

    adding the project basic structure.

---
+++ core/src/main/java/hailu/vaadin/authentication/core/App.java
@@ -0,0 +1,13 @@
+package hailu.vaadin.authentication.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
